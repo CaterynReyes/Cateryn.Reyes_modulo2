1e634704b28784f0fcd2d5f045f625934c9635ab
CaterynReyes <115513025+CaterynReyes@users.noreply.github.com>
2022-12-07 15:47:46 -0600

    Clase 2
    tarea bird

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,7 +1,10 @@
-from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
 import pygame
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS, RUNNING
 from dino_runner.components.dinosaur import Dinosaur
+from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
+
+
+
 
 
 class Game:
@@ -10,36 +13,49 @@ class Game:
         pygame.display.set_caption(TITLE)
         pygame.display.set_icon(ICON)
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
         self.clock = pygame.time.Clock()
         self.playing = False
         self.game_speed = 20
         self.x_pos_bg = 0
         self.y_pos_bg = 380
         self.player = Dinosaur()
-        self.obstacle_manager = ObstacleManager
+        self.obstacle_manager = ObstacleManager()
+
+    
 
     def run(self):
         # Game loop: events - update - draw
+        self.obstacle_manager.reset_obstacles(self) 
         self.playing = True
         while self.playing:
-            self.events()
-            self.update()
+           # self.screen.fill(color=0)   # Color change flickering
+            self.events()           
+            self.update()       
             self.draw()
+
+
+    def execute(self):
+        while self.running:
+            
+            if not self.playing:
+                self.show_menu()       
 
     def events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.playing = False
+                self.running = False
+            self.screen.fill((255, 255, 255))
 
     def update(self):
         user_input = pygame.key.get_pressed()
         self.player.update(user_input)
-        self.obstacle_manager.update(self.game_speed)
-       
-
+        self.obstacle_manager.update(self)
+        
     def draw(self):
-        self.clock.tick(FPS)
-        self.screen.fill((255, 255, 255))
+        self.screen.fill(color=(255,255,104))   #  Color change     
+        self.clock.tick(FPS) 
         self.draw_background()
         self.player.draw(self.screen)
         self.obstacle_manager.draw(self.screen)
--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,36 +1,59 @@
-import random
-
-import pygame
-from dino_runner.components.obstacles.cactus import  LargeCactus, SmallCactus
+from dino_runner.components.obstacles.cactus import SmallCactus, LargeCactus
+from dino_runner.components.obstacles.Bird import Bird
 from dino_runner.utils.constants import (
-    SMALL_CACTUS,LARGE_CACTUS
+    SMALL_CACTUS, LARGE_CACTUS, BIRD
 )
+from dino_runner.components.dinosaur import Dinosaur
+import random
+import  pygame
+
+
 class ObstacleManager:
 
     def __init__(self):
-        self.obstacles=[]
+        self.obstacles = []
 
-    def update(self,game):
-        if len(self.obstacles)==0:
-            cactus_size=random.randint(0,2)
-            if cactus_size==0:
+
+    def update(self, game):
+        #if len(self.obstacles) == 0:
+        #    cactus_size = random.randint(0,2)
+        #    if cactus_size == 0:
+        #        self.obstacles.append(LargeCactus(LARGE_CACTUS))
+        #    elif cactus_size ==1:
+        #        self.obstacles.append(SmallCactus(SMALL_CACTUS))
+             
+        if len(self.obstacles) == 0:
+            if random.randint(0, 2) == 0:
                 self.obstacles.append(SmallCactus(SMALL_CACTUS))
-            else:
+            elif random.randint(0, 2) == 1:
                 self.obstacles.append(LargeCactus(LARGE_CACTUS))
+            elif random.randint(0, 2) == 2:
+                self.obstacles.append(Bird(BIRD))
 
+                    
+        
         for obstacle in self.obstacles:
-            obstacle.update(game.game_speed,self.obstacles)
+            obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
-                game.playing = False
-                pygame.time.delay(1000)
-                break
-                
+                pygame.time.delay(100)
+                self.obstacles = []
 
-                          
-    def draw(self,screen):
+                game.player_heart_manager.reduce_heart()
+                if game.player_heart_manager.heart_count > 0:
+                    game.player.show_text = False
+
+                else:
+                    pygame.time.delay(500)
+                    game.playing = False
+                    game.death_count += 1
+                    break
+
+
+    def draw(self, screen):
         for obstacle in self.obstacles:
             obstacle.draw(screen)
 
-    def reset_obstacles(self,self1):
-        self.obstacles=[]
+    def reset_obstacles(self, self1):
+        self.obstacles = []
+        
         
